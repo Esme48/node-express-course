873b7eefee3f8653badab3117f7c35c069f39c01
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-04-30 07:45:10 -0700

    week7

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -3,17 +3,8 @@ const router = express.Router()
 
 const { addPerson, getPeople, getPersonById, updatePersonId, deletePerson } = require("../controllers/people.js");
 
-router.get('/', getPeople)// (req, res)=>{
-  //  res.status(200).json({success: true, data: people})
-    //})
-router.post('/', addPerson)//(req, res) =>{
-    //const {name}= req.body
-   // if(!name){
-   //     return res.status(400).json({ success: false, message: "Please provide a name"})
-   // }
-   // people.push({id: people.length + 1, name: req.body.name})
-  //  res.status(201).json({ success: true, name: req.body.name})
-    //})
+router.get('/', getPeople)
+router.post('/', addPerson)
 router.get('/:id', getPersonById)
 router.put('/:id', updatePersonId)
 router.delete('/:id', deletePerson)
